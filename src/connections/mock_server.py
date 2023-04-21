5ac335ba62f7c1ecd45ae95caf37553a436d7836
felixsundman <81686723+felixsundman@users.noreply.github.com>
2023-04-21 11:15:58 +0200

    made another oopsie:(

--- src/connections/mock_server.py
+++ src/connections/mock_server.py
@@ -1,77 +1,31 @@
 import asyncio
 import socketio
-import json
-
 '''''''''
 This test server listens for incoming connections, and when a client connects, it stores its SID(session ID). 
 You can send hard-coded commands to the connected client by typing them in the terminal(e.g., forward, backward, left, right). 
 The server will send the command as a MOWER_COMMAND event to the connected client.
 '''''''''
-
-# Setup Socket.IO server
-sio = socketio.AsyncServer(async_mode='aiohttp')
-app = socketio.AsyncApp(server=sio)
-
-# Store the connected mower's SessionID
-mower_sid = None
-
-# Event handler for new connections
+sio = socketio.AsyncServer(async_mode='asgi')
+app = socketio.ASGIApp(sio)
 
 
 @sio.event
 async def connect(sid, environ):
-    global mower_sid
-    print(f"Client connected: {sid}")
-    mower_sid = sid
-
-# Event handler for disconnections
+    print('Client connected:', sid)
 
 
 @sio.event
 async def disconnect(sid):
-    global mower_sid
-    print(f"Client disconnected: {sid}")
-    if sid == mower_sid:
-        mower_sid = None
-
-# Event handler for incoming messages
+    print('Client disconnected:', sid)
 
 
-@sio.on("message")
+@sio.event
 async def message(sid, data):
-    print(f"Message from {sid}: {data}")
+    print(f"Received message from {sid}: {data}")
 
-
-async def send_command(command):
-    global mower_sid
-    if mower_sid:
-        data = {
-            "type": "MOWER_COMMAND",
-            "data": {
-                "direction": command
-            }
-        }
-        await sio.emit("message", json.dumps(data), to=mower_sid)
-    else:
-        print("Mower is not connected.")
-
-
-async def main():
-    # Start the server
-    runner = socketio.AsyncRunner(app, host='localhost', port=8080)
-    await runner.setup()
-    await runner.server.start_runner()
-
-    try:
-        while True:
-            command = input("Enter command (forward, backward, left, right): ")
-            if command in ["forward", "backward", "left", "right"]:
-                await send_command(command)
-            else:
-                print("Invalid command. Try again.")
-    except KeyboardInterrupt:
-        print("Shutting down server...")
-        await runner.server.cleanup()
+    # Example of sending a hardcoded command to the client
+    await sio.emit('message', '{"type": "MOWER_COMMAND", "data": {"direction": "left"}}', to=sid)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=3000)
