dcad7ecdb3587a0b8cda704c2240bd8f787b3795
baan20ty <baan20ty@student.ju.se>
2023-04-25 15:51:10 +0200

    Added simple websocket solution

--- src/main.py
+++ src/main.py
@@ -5,42 +5,31 @@ import logging
 # File imports
 import Connector
 from websocket_client import WebSocketClient
-from module.Camera import Camera
+#from module.Camera import Camera
 
 
-
-
-# Main program
 def main():
     con = Connector.Connector()
-    
-    websocket_client = WebSocketClient()
+
+    websocket_client = WebSocketClient(con)
     websocket_client.start()
 
-    camera = Camera()
-    
+   # camera = Camera()
+
     if con.connected:
         try:
             while True:
-                con.forward()
-  
                 data = con.read_data()
 
                 if data == "CAPTURE":
                     print('Object detected! Capturing Image...')
-
-                    camera.capture("test-image.jpg")
-
-                # camera.start_preview()
-                # camera.capture("test-image.jpg")
-                # camera.start_preview()
+                    #camera.capture("test-image.jpg")
 
         except KeyboardInterrupt:
             logging.info("Keyboard interrupt received, stopping...")
             # Gracefully stop the motors on KeyboardInterrupt
             con.stop()
             con.close()
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
--- src/websocket_client.py
+++ src/websocket_client.py
@@ -1,60 +1,45 @@
 import websocket
-import json
-import threading
-
-SERVER_URL = "ws://localhost:8080"
-
-'''''''''''
-1. Initializes a WebSocketApp object with the specified server URL and sets up the event handlers for the WebSocket connection.
-2. Starts the WebSocket connection in a separate thread to prevent blocking the main thread.
-3. Sends initial data to the server when the connection is opened.
-4. Receives and handles initial data from the server.
-5. Handles errors and connection closure events.
-'''''''''
-
+from threading import Thread
 
 class WebSocketClient:
-    def __init__(self):
-        self.ws = websocket.WebSocketApp(SERVER_URL,
-                                         on_message=self.on_message,
-                                         on_error=self.on_error,
-                                         on_close=self.on_close
-                                         )
-        self.ws.on_open = self.on_open
+    def __init__(self, connector):
+        self.connector = connector
+        self.ws_url = "ws://localhost:8080" # Replace with your WebSocket server URL
+        self.ws = None
 
-    def start(self):
-        # Start the websocket connection in a separate thread
-        thread = threading.Thread(target=self.ws.run_forever)
-        thread.daemon = True
-        thread.start()
-
-    def on_open(self, *args):
-        print("WebSocket connection opened")
-
-        # Send initial data to the server
-        initial_data = {"type": "initial_data", "data": "Hello, server!"}
-        self.send(json.dumps(initial_data))
-
-    def on_message(self, *args, **kwargs):
-        message = args[1]
+    def on_message(self, message):
         print(f"Received message: {message}")
 
-        # Handle received initial data from the server
-        message_data = json.loads(message)
-        if message_data.get("type") == "initial_data":
-            print("Received initial data from the server")
+        if message == "w":
+            self.connector.forward()
+        elif message == "s":
+            self.connector.backward()
+        elif message == "a":
+            self.connector.left()
+        elif message == "d":
+            self.connector.right()
+        elif message == "x":
+            self.connector.stop()
+        elif message == "q":
+            self.connector.drive_autonomously()
 
-    def on_error(self, *args):
-        error = args[1]
+    def on_error(self, error):
         print(f"WebSocket error: {error}")
 
-    def on_close(self, *args):
-        print("WebSocket connection closed")
+    def on_close(self):
+        print("WebSocket closed")
 
-    def send(self, message):
-        self.ws.send(message)
+    def on_open(self):
+        print("WebSocket opened")
 
+    def start(self):
+        self.ws = websocket.WebSocketApp(
+            self.ws_url,
+            on_message=lambda ws, msg: self.on_message(msg),
+            on_error=lambda ws, err: self.on_error(err),
+            on_close=lambda ws: self.on_close(),
+        )
 
-if __name__ == "__main__":
-    client = WebSocketClient()
-    client.start()
+        self.ws.on_open = self.on_open
+        thread = Thread(target=self.ws.run_forever)
+        thread.start()
