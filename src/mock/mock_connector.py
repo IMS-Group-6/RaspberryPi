35d332871f835844ba02eb709707a493b59b6d52
h-khalaf <h.khalaf723@gmail.com>
2023-05-04 22:01:37 +0200

    set up a mock connection to test the code

---
+++ src/mock/mock_connector.py
@@ -0,0 +1,45 @@
+class MockConnector:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._configure_serial_connection()
+        return cls._instance
+
+    def _configure_serial_connection(self):
+        self.connected = True
+
+    def close(self):
+        if self.connected:
+            print("close")
+
+    def __del__(self):
+        self.close()
+
+    def send_command(self, cmd):
+        print(cmd)
+
+    def forward(self):
+        self.send_command("forward")
+
+    def backward(self):
+        self.send_command("backward")
+
+    def left(self):
+        self.send_command("left")
+
+    def right(self):
+        self.send_command("right")
+
+    def stop(self):
+        self.send_command("stop")
+
+    def drive_autonomously(self):
+        self.send_command("auto")
+
+    def start(self):
+        self.send_command("start")
+
+    def manual(self):
+        self.send_command("stop")
--- src/test.py
+++ src/test.py
@@ -6,26 +6,41 @@ from time import sleep
 # File imports
 from connections.api_client import APIClient
 from command_handler import CommandHandler
+from mock.mock_connector import MockConnector
 import config
 
 def main():
     api_client = APIClient()
+    con = MockConnector()
 
-    if True:
+    if con.connected:
         while True:
             print(f"Sending GET /ping to {config.SERVER_URL}")
-            api_client.ping()
-            sleep(10)
+
+            if api_client.ping():
+                print("Successful...")
+            else:
+                print("Failed...")
+            
+            sleep(30)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    """
+    This script sets up a mock connection to test the code without access to hardware. 
 
+    Note that this test is still dependent on a backend connection, but it allows for testing the code in a 
+    controlled environment without the need for actual hardware. It focuses on testing the socket.io commands.
+    """
     t = threading.Thread(target=main, daemon=True)
     t.start()
     
-    con = "test"
+    con = MockConnector()
     command_handler = CommandHandler(connector=con)
     try:
         asyncio.run(command_handler.listen())
     except asyncio.exceptions.CancelledError:
         logging.info("Keyboard interrupt received, stopping...")
+        if (con.connected):
+            print("\n----")
+            con.stop()
+            con.close()
