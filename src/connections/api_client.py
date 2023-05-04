4298ebb8aaafcb428266c9b159702867a55b6953
h-khalaf <h.khalaf723@gmail.com>
2023-05-04 02:17:16 +0200

    api client is now a singleton class

--- src/connections/api_client.py
+++ src/connections/api_client.py
@@ -3,8 +3,14 @@ import logging
 import config
 
 class APIClient:
-    def __init__(self):
-        self.server_url = config.SERVER_URL
+    server_url = config.SERVER_URL
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.server_url = config.SERVER_URL
+        return cls._instance
     
     def ping(self):
         """
